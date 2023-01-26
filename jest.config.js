514201ef83ba152490951e5f085c7f33184f0fc0
AndreiZernov <andrei.zernov@outlook.com>
2023-01-26 11:11:40 +0000

    refactor: remove moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,6 @@ const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-  },
   testEnvironment: 'jest-environment-jsdom',
 };
 
